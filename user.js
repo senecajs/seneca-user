1f35dec15387b575e6f69144b10fa51fae3e2966
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-22 21:55:29 +0300

    changes for role: user, get: user command response structure

--- user.js
+++ user.js
@@ -278,7 +278,10 @@ module.exports = function user(options) {
 
 
   function cmd_load_user( args, done ) {
-    seneca.make(user_canon).load$(_.omit( args, ['role','get'] ), done)
+    seneca.make(user_canon).load$(_.omit( args, ['role','get'] ), function(err, user){
+      if (err) return done(err)
+      return done(null, {ok: true, user: user})
+    })
   }
 
   // Action Implementations
