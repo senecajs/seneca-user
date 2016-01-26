a3b38fe05161f7d7b2c737a56a6cfccdbc8b755e
malex <mircea.alexandru@gmail.com>
2016-01-26 17:04:59 +0200

    update user with password report error

--- user.js
+++ user.js
@@ -974,7 +974,7 @@ module.exports = function user (options) {
       if (pwd) {
         if (pwd === pwd2 && 1 < pwd.length) {
           // FIX: needs to a proper action call
-          cmd_change_password({nick: args.orig_nick, password: pwd}, function (err, userpwd) {
+          seneca.act('role: ' + role + ', cmd: change_password', {nick: args.orig_nick, password: pwd}, function (err, userpwd) {
             if (err) return done(err, {ok: false, why: err})
             user = userpwd
           })
