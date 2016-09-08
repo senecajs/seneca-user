eeda223f76c1b61de1e8b27cb7d412f173e71e2c
Emer Rutherford <emer.rutherford@nearform.com>
2016-09-08 10:04:00 +0100

    Changed paramter names in basic.js

--- doc/examples/basic.js
+++ doc/examples/basic.js
@@ -3,28 +3,28 @@ seneca.use(require('seneca-basic'))
 seneca.use(require('seneca-entity'))
 seneca.use(require('seneca-user'))
 
-seneca.ready(() => {
+seneca.ready((msg, done) => {
   seneca.act({role: 'user', cmd: 'register', name: "Flann O'Brien", email: 'nincompoop@deselby.com', password: 'blackair'},
-  function (err, out) {
-    if (err) {
-      console.log('err: ', err)
-      return
-    }
-    seneca.act({role: 'user', cmd: 'login', email: 'nincompoop@deselby.com', password: 'bicycle'}, function (err, out) {
-      if (err) {
-        console.log('err: ', err)
-        return
-      }
-      console.log('login success: ' + out.ok)
+   (msg, done) => {
+     if (msg) {
+       console.log('err: ', msg)
+       return
+     }
+     seneca.act({role: 'user', cmd: 'login', email: 'nincompoop@deselby.com', password: 'bicycle'}, (msg, done) => {
+       if (msg) {
+         console.log('err: ', msg)
+         return
+       }
+       console.log('login success: ' + done.ok)
 
-      seneca.act({role: 'user', cmd: 'login', email: 'nincompoop@deselby.com', password: 'blackair'}, function (err, out) {
-        if (err) {
-          console.log('err: ', err)
-          return
-        }
-        console.log('login success: ' + out.ok)
-        console.log('login instance: ' + out.login)
-      })
-    })
-  })
+       seneca.act({role: 'user', cmd: 'login', email: 'nincompoop@deselby.com', password: 'blackair'}, (msg, done) => {
+         if (msg) {
+           console.log('err: ', msg)
+           return
+         }
+         console.log('login success: ' + done.ok)
+         console.log('login instance: ' + done.login)
+       })
+     })
+   })
 })
